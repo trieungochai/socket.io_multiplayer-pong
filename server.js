ba7e3ce63260115ac8f9fd4a49ae5ce02b2f48fa
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-14 11:24:15 +0700

    Implementing the Game logic: Ball

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ io.on("connection", (socket) => {
   socket.on("paddleMove", (paddleData) => {
     socket.broadcast.emit("paddleMove", paddleData);
   });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 });
 
 server.listen(PORT, () => {
